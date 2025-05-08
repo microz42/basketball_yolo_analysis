7ab1d44c6e8f64945cec17c45bcc244ecdd35c03
gaba42 <106816837+gaba42@users.noreply.github.com>
2025-05-08 21:55:29 +0900

    basketball draw triangle pointer

--- drawers/utils.py
+++ drawers/utils.py
@@ -1,7 +1,22 @@
 import cv2
+import numpy as np
 import sys
 sys.path.append("../")
 from utils import get_bbox_width, get_center_of_bbox
+
+
+def draw_triangle(frame, bbox, color):
+    y = int(bbox[1])
+    x,_ = get_center_of_bbox(bbox)
+
+    triangle_points = np.array([
+        [x,y],
+        [x-10,y-20],
+        [x+10,y-20]
+    ])
+    cv2.drawContours(frame, [triangle_points], 0, color, cv2.FILLED)
+    cv2.drawContours(frame, [triangle_points], 0, (0,0,0), 2)  # borders
+    return frame
 
 
 def draw_ellipse(frame, bbox, color, track_id=None):
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from utils import read_video, save_video
-from trackers import PlayerTracker
+from trackers import PlayerTracker, BallTracker
 from drawers import(
-    PlayerTracksDrawer
+    PlayerTracksDrawer,
+    BallTracksDrawer
 )
 
 
@@ -12,6 +13,7 @@ def main():
 
     # Initialize Tracker
     player_tracker = PlayerTracker("models/player_detector.pt")
+    ball_tracker = BallTracker("models/ball_detector_model.pt")
 
     # run tracker
     player_tracks = player_tracker.get_object_tracks(video_frames,
@@ -19,13 +21,19 @@ def main():
                                                      stub_path = "stubs/player_track_stubs.pkl"
                                                      )
 
+    ball_tracks = ball_tracker.get_object_tracks(video_frames,
+                                                     read_from_stub=True,
+                                                     stub_path = "stubs/ball_track_stubs.pkl"
+                                                     )
 
     # draw output
     # initialize drawer
     player_tracks_drawer = PlayerTracksDrawer()
+    ball_tracks_drawer = BallTracksDrawer()
 
     # draw object tracks
     output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks)
+    output_video_frames = ball_tracks_drawer.draw(output_video_frames, ball_tracks)
 
     # save video
     save_video(output_video_frames, "output_videos/output_video.avi")
